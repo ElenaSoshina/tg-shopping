05bb1fc516ee5b132028aa47779ce80063b31920
Elena <elena@Elenas-MacBook-Pro.local>
2025-01-22 15:16:30 +0700

    add id parametre in db.js

--- src/db/db.js
+++ src/db/db.js
@@ -8,12 +8,12 @@ import waterImg from '../images/water.png'
 
 export function getData() {
     return [
-        {title: 'Burger', price: 15, image: burgerImg},
-        {title: 'Pizza', price: 17.99, image: pizzaImg},
-        {title: 'Coca', price: 3.5, image: cocaImg},
-        {title: 'IceCream', price: 2.99, image: icecreamImg},
-        {title: 'Kebab', price: 13.99, image: kebabImg},
-        {title: 'Salad', price: 2.50, image: saladImg},
-        {title: 'Water', price: 0.99, image: waterImg},
+        {id: 1, title: 'Burger', price: 15, image: burgerImg},
+        {id: 2, title: 'Pizza', price: 17.99, image: pizzaImg},
+        {id: 3, title: 'Coca', price: 3.5, image: cocaImg},
+        {id: 4, title: 'IceCream', price: 2.99, image: icecreamImg},
+        {id: 5, title: 'Kebab', price: 13.99, image: kebabImg},
+        {id: 6, title: 'Salad', price: 2.50, image: saladImg},
+        {id: 7, title: 'Water', price: 0.99, image: waterImg},
     ]
 }
