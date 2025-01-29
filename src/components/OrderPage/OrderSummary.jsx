a6935af7c8bb1152cfcf448d8cd5fc6b4812c53a
Elena <elena@Elenas-MacBook-Pro.local>
2025-01-29 15:09:27 +0700

    ch

--- src/components/OrderPage/OrderSummary.jsx
+++ src/components/OrderPage/OrderSummary.jsx
@@ -7,14 +7,14 @@ const OrderSummary = ({ orderItems, totalPrice, type }) => (
         <div className="order-summary">
             <span>Итого: {Number(totalPrice).toLocaleString('ru-RU')} VND</span>
         </div>
-        {type !== 'fish' && (
-            <p>
-                <strong>Топпинги:</strong>{' '}
-                {orderItems[0]?.toppings?.length > 0
-                    ? mapToppingNames(orderItems[0].toppings).join(', ')
-                    : 'Нет'}
-            </p>
-        )}
+        {/*{type !== 'fish' && (*/}
+        {/*    <p>*/}
+        {/*        <strong>Топпинги:</strong>{' '}*/}
+        {/*        {orderItems[0]?.toppings?.length > 0*/}
+        {/*            ? mapToppingNames(orderItems[0].toppings).join(', ')*/}
+        {/*            : 'Нет'}*/}
+        {/*    </p>*/}
+        {/*)}*/}
     </div>
 );
 
