9e4560cf78c89508320ff8a409864709d4efcaf7
Elena <elena@Elenas-MacBook-Pro.local>
2025-01-30 11:41:43 +0700

    changes in OrderSummsry

--- src/components/OrderPage/OrderSummary.jsx
+++ src/components/OrderPage/OrderSummary.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 // import { mapToppingNames } from '../../utils/utils';
 import './OrderSummary.css'; // Используем отдельный CSS файл
 
-const OrderSummary = ({ orderItems, totalPrice, type }) => (
-    <div className="order-summary-container">
-        <div className="order-summary">
-            <span>Итого: {Number(totalPrice).toLocaleString('ru-RU')} VND</span>
+const OrderSummary = ({ orderItems, totalPrice, type }) => {
+    const finalPrice = type === 'fish' ? totalPrice / 100 : totalPrice;
+    return (
+        <div className="order-summary-container">
+            <div className="order-summary">
+                <span>Итого: {Number(totalPrice).toLocaleString('ru-RU')} VND</span>
+            </div>
         </div>
-        {/*{type !== 'fish' && (*/}
-        {/*    <p>*/}
-        {/*        <strong>Топпинги:</strong>{' '}*/}
-        {/*        {orderItems[0]?.toppings?.length > 0*/}
-        {/*            ? mapToppingNames(orderItems[0].toppings).join(', ')*/}
-        {/*            : 'Нет'}*/}
-        {/*    </p>*/}
-        {/*)}*/}
-    </div>
-);
+    )
+    }
+
+;
 
 export default OrderSummary;
