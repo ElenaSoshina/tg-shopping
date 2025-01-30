551f1e5eab56c9ed972b4c69c8cecd49007af053
Elena <elena@Elenas-MacBook-Pro.local>
2025-01-30 16:14:08 +0700

    Updates

--- src/components/OrderPage/OrderImage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './OrderPage.css'
-
-const OrderImage = ({ style }) => {
-    return <div className="order-image" style={style}></div>;
-};
-
-export default OrderImage;
